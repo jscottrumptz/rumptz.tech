6f6a4afe53ec19f59015cfd6f8b1c34c28bca3d3
J Scott Rumptz <jscott@rumptz.tech>
2021-04-12 12:19:59 -0500

    added resume to nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -43,7 +43,7 @@ function Nav() {
                                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Portfolio</a>
 
                             <a href="#"
-                               className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Availability</a>
+                               className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Resume</a>
                         </div>
                     </div>
                     <div className="flex items-center">
@@ -74,7 +74,7 @@ function Nav() {
                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Portfolio</a>
 
                     <a href="#"
-                       className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Availability</a>
+                       className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Resume</a>
 
                 </div>
             </div>
