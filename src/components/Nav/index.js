e1a02d22f9db6fa8b97280222a68417afb80091f
J Scott Rumptz <jscott@rumptz.tech>
2021-04-14 09:34:33 -0500

    tweaked hamburger button on mobile

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,7 +16,7 @@ function Nav(props) {
                 <div className="flex justify-between h-16">
                     <div className="flex">
                         <div className="-ml-2 mr-2 flex items-center md:hidden">
-                            <a onClick={() => setHidden('px-2 pt-2 pb-3 space-y-1 sm:px-3')}>
+                            <a onClick={() => setHidden('px-2 pt-2 pb-3 space-y-1 sm:px-3')} onBlur={() => {setHidden('hidden')}}>
                             <button type="button"
                                     className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                                     aria-controls="mobile-menu" aria-expanded="false">
